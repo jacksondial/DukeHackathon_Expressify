51255280ab6ade2e04402c9bdc51d00fa8c74bf1
Namratha Prithviraj <namrathaprithviraj@gmail.com>
2022-10-22 18:19:49 -0400

    initial netflix subtitle styler

---
+++ popup.js
@@ -0,0 +1,29 @@
+const form = document.getElementById("popup-form");
+const inputElements = ["vPos", "fSize", "fColor"];
+
+chrome.storage.local.get(inputElements, data => {
+  inputElements.forEach(el => {
+    document.getElementById(el).value = data[el];
+  });
+});
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+  const [vPos, fSize, fColor] = [...inputElements.map(el => event.target[el].value)];
+
+  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+    chrome.storage.local.set({ vPos, fSize, fColor });
+    chrome.scripting.executeScript(
+      {
+        target: {tabId: tabs[0].id},
+        files: ["script.js"]
+      },
+      () => {
+        const error = chrome.runtime.lastError;
+        if (error) "Error. Tab ID: " + tab.id + ": " + JSON.stringify(error);
+
+        chrome.tabs.sendMessage(tabs[0].id, { vPos, fSize, fColor });
+      }
+    );
+  });
+});
---
+++ script.js
@@ -0,0 +1,52 @@
+chrome.runtime.onMessage.addListener((message, _sender, _sendResponse) => {
+    changeSubtitlesStyle(message.vPos, message.fSize, message.fColor);
+  });
+  
+  changeSubtitlesStyle = (vPos, fSize, fColor) => {
+    console.log("%cnetflix-subtitles-styler : observer is working... ", "color: red;");
+    callback = () => {
+      // .player-timedText
+      const subtitles = document.querySelector(".player-timedtext");
+      if (subtitles) {
+        subtitles.style.bottom = vPos + "px";
+  
+        // .player-timedtext > .player-timedtext-container [0]
+        const firstChildContainer = subtitles.firstChild;
+        if (firstChildContainer) {
+          // .player-timedtext > .player-timedtext-container [0] > div
+          const firstChild = firstChildContainer.firstChild;
+          if (firstChild) {
+            firstChild.style.backgroundColor = fColor;
+
+            console.log(firstChild.textContent)
+
+            // for (const span of firstChild.childNodes) {
+            //     // .player-timedtext > .player-timedtext-container [1] > span
+            //     console.log(span.textContent)
+            //   }
+          }
+  
+          // .player-timedtext > .player-timedtext-container [1]
+          const secondChildContainer = firstChildContainer.nextSibling;
+          if (secondChildContainer) {
+            for (const span of secondChildContainer.childNodes) {
+              // .player-timedtext > .player-timedtext-container [1] > span
+              span.style.fontSize = fSize + "px";
+              span.style.fontWeight = "normal";
+              span.style.color = fColor;
+              console.log(span)
+            }
+            secondChildContainer.style.left = "0";
+            secondChildContainer.style.right = "0";
+          }
+        }
+      }
+    };
+  
+    const observer = new MutationObserver(callback);
+    observer.observe(document.body, {
+      subtree: true,
+      attributes: false,
+      childList: true
+    });
+  };
